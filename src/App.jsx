13b53103b56a87cc9ae1a9de11f904be4e6b2aca
KHELIFI Ahmed Aziz <ahmedazizkhelifi@gmail.com>
2023-01-13 18:47:31 +0100

    config

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+import Broh from "./components/broh"
+
+function App() {
+  return (
+    <div className="App">
+      <Broh />
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
