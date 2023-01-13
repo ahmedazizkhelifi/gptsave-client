73372ac12f847be65ef57b10688c0252373cff46
KHELIFI Ahmed Aziz <ahmedazizkhelifi@gmail.com>
2023-01-13 19:12:08 +0100

    landing page

---
+++ src/pages/landing/landing.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./landing.css"
+import { Navbar } from "../../components"
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <div> aze </div>
+    </>
+  )
+}
+
+export default Landing
