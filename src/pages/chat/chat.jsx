499ab6e0e61f5f12bca8d47f5d535aea3b8fac47
KHELIFI Ahmed Aziz <ahmedazizkhelifi@gmail.com>
2023-01-14 01:03:12 +0100

    chat component

--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { Navbar, Loading } from "../../components"
+import { Navbar, Loading, Tag } from "../../components"
 import { Notfound } from "../../pages"
 import "./chat.css"
 import "./global-chat.css"
 const Chat = () => {
   const { chatId } = useParams()
   const [data, setData] = useState(null)
+  const [content, setContent] = useState(null)
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     // request a page
-
+    fetch(`http://localhost:3010/api/chat/${chatId}`)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json()
+        } else {
+          throw new Error(response.status)
+        }
+      })
+      .then((data) => {
+        console.log(data)
+        setData(data)
+      })
+      .catch((error) => {
+        console.log(error)
+        setData({ error: "failed" })
+      })
     fetch(`https://gptsave.blob.core.windows.net/newcontainer/${chatId}.html`)
       .then((response) => {
         if (response.status === 200) {
@@ -21,7 +37,7 @@ const Chat = () => {
         }
       })
       .then((data) => {
-        setData(data)
+        setContent(data)
         setLoading(false)
       })
       .catch((error) => {
@@ -29,13 +45,12 @@ const Chat = () => {
         setError(true)
         setLoading(false)
       })
-  }, [chatId])
+  }, [])
 
   return (
     <>
       <Navbar />
       <div className="chat section__margin section__padding">
-        <h1>chat {chatId}</h1>
         {/* 
         owner:
         likes dislikes
@@ -47,7 +62,24 @@ const Chat = () => {
         ) : error ? (
           <Notfound />
         ) : (
-          <div dangerouslySetInnerHTML={{ __html: data }} />
+          <>
+            <h2>{data?.title ?? "Loading ..."}</h2>
+            <p>
+              By: <strong>{data?.owner}</strong>
+            </p>
+            {data?.tags?.map((tag, index) => (
+              // <span key={index} className="tag">
+              //   {tag}
+              // </span>
+              <Tag
+                style={{ marginLeft: 10 }}
+                key={index}
+                tag={tag}
+                tagType="three"
+              />
+            ))}
+            <div dangerouslySetInnerHTML={{ __html: content }} />
+          </>
         )}
       </div>
     </>
